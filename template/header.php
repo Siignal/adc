13467b30ebe414448e78075ff5e9991bd0d7453a
Siignal <anthony.77181@hotmail.fr>
2017-04-04 18:15:28 +0200

    Add files via upload

---
+++ template/header.php
@@ -0,0 +1,40 @@
+<header>
+  <div id="content" style="padding: 10px 0 0 0;">
+    <h1 class="logo"></h1>
+  </div>
+</header>
+ 
+<nav id="menu" class="navigation">
+  <div id="content">
+    <ul>
+      <li class="active" >
+        <a href="me">Siignal</a>
+      </li>
+      <li>
+        <a href="community">Communaut&eacute;</a>
+      </li>
+      <li>
+        <a href="shop">Boutique</a>
+      </li>
+      <li>
+        <a href="admin/"  target="_blank">Administration</a>
+      </li>
+    </ul>
+  </div>
+</nav>  
+
+<div id="content_sub">
+  <div id="subnav" class="navigation">
+    <ul>
+      <li>
+        <a class="active" href="me">Siignal</a>
+      </li>
+      <li>
+          <a  href="home">Home</a> 
+      </li>
+      <li>
+        <a href="parametre">Paramètres</a>
+      </li>
+    </ul>
+  </div>
+</div>
