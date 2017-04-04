5cc4d564c469267103dcff9182b8781f9c0e9853
Siignal <anthony.77181@hotmail.fr>
2017-04-04 18:10:29 +0200

    Add files via upload

---
+++ me.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="author" content="Siignal" />
+
+<title>Habboo: Siignal</title>
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+
+<!-- StyleSheets -->
+<link rel="stylesheet" href="assets/css/global.css" >
+
+<!-- Javascript -->
+
+
+</head>
+<body>
+
+<?php include('template/header.php'); ?>
+
+
+</body>
+</html>
