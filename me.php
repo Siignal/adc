63bf20b23d041597769677a3a8b8d8c429d3085a
Mohinette <mohinette@hotmail.fr>
2017-04-05 13:22:30 +0200

    Add files via upload

--- me.php
+++ me.php
@@ -7,8 +7,53 @@
 		<div id='article' style="background-image:url(https://images.habbo.com/web_images/habbo-web-articles/lpromo_easter17_gen2.png)">
 
 		</div>
+		<div id='sub--article__l'>
+			<div id='mini--article' style="background-image:url(https://images.habbo.com/web_images/habbo-web-articles/lpromo_easter17_gen2.png)">
+				<div id='mini--infos'>
+					<a href='' class='news__lien'><h2 class='news__title'>Titre</h2></a>
+					<time class='news__date'>5 Avr. 2017</time><a href='#' class='news__tags'>Updates</a>
+					<p class='news__summary'>Description</p>
+				</div>
+			</div>
+			<div id='mini--article' style="background-image:url(https://images.habbo.com/web_images/habbo-web-articles/lpromo_easter17_gen2.png)">
+				<div id='mini--infos'>
+					<a href='' class='news__lien'><h2 class='news__title'>Titre</h2></a>
+					<time class='news__date'>5 Avr. 2017</time><a href='#' class='news__tags'>Updates</a>
+					<p class='news__summary'>Description</p>
+				</div>
+			</div>
+			<div id='mini--article' style="background-image:url(https://images.habbo.com/web_images/habbo-web-articles/lpromo_easter17_gen2.png)">
+				<div id='mini--infos'>
+					<a href='' class='news__lien'><h2 class='news__title'>Titre</h2></a>
+					<time class='news__date'>5 Avr. 2017</time><a href='#' class='news__tags'>Updates</a>
+					<p class='news__summary'>Description</p>
+				</div>
+			</div>
+		</div>
+		<div id='sub--article__r'>
+			<div id='mini--article' style="background-image:url(https://images.habbo.com/web_images/habbo-web-articles/lpromo_easter17_gen2.png)">
+				<div id='mini--infos'>
+					<a href='' class='news__lien'><h2 class='news__title'>Titre</h2></a>
+					<time class='news__date'>5 Avr. 2017</time><a href='#' class='news__tags'>Updates</a>
+					<p class='news__summary'>Description</p>
+				</div>
+			</div>
+			<div id='mini--article' style="background-image:url(https://images.habbo.com/web_images/habbo-web-articles/lpromo_easter17_gen2.png)">
+				<div id='mini--infos'>
+					<a href='' class='news__lien'><h2 class='news__title'>Titre</h2></a>
+					<time class='news__date'>5 Avr. 2017</time><a href='#' class='news__tags'>Updates</a>
+					<p class='news__summary'>Description</p>
+				</div>
+			</div>
+			<div id='mini--article' style="background-image:url(https://images.habbo.com/web_images/habbo-web-articles/lpromo_easter17_gen2.png)">
+				<div id='mini--infos'>
+					<a href='' class='news__lien'><h2 class='news__title'>Titre</h2></a>
+					<time class='news__date'>5 Avr. 2017</time><a href='#' class='news__tags'>Updates</a>
+					<p class='news__summary'>Description</p>
+				</div>
+			</div>
+		</div>
 	</div>
 	<div id='right'>aaaaaaaa</div>
 </div>
 
-<?php include('template/footer.php'); ?>
