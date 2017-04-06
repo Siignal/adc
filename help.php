284689efc2a2154f6e4c67437effbc9ff8b011e7
Siignal <anthony.77181@hotmail.fr>
2017-04-06 18:52:46 +0200

    Add files via upload

--- help.php
+++ help.php
@@ -3,7 +3,68 @@ $pageid = '11'; ?>
 <?php include('template/header.php'); ?>
 
 <div id='content'>
-	<div id='left'></div>
+	<div id='left'>
+		<h1>COMMENT FAIRE FACE À UN PROBLÈME SUR HABBOO?</h1>
+		<p>Tu pourras de temps en temps avoir à faire à de mauvaises conduites sur Habboo. Mais ne crains rien, l'aide est à portée de main! Sur cette page, nous allons t'expliquer quels sont les outils qui fonctionnent le mieux pour faire face à des situations délicates.</p>
+<hr>
+		<h2>DANS UN APPART</h2>
+		<p>Quand tu es dans un appart et qu'un autre Habboo apparaît comme hors-ligne, clique sur son avatar pour faire apparaître un menu déroulant. Tu pourras ainsi l'ignorer, modérer ou même rapporter ce parturbateur.</p>
+
+		<h3>Ignorer un Habbo</h3>
+		<p>Si <strong>un Habbo dit des choses qui peuvent te mettre mal à l'aise</strong>, tu peux les ignorer. C'est la solution idéale contre les taquineries, le spam, ou tout simplement quand tu veux lui dire "Hasta la vista" mais tu ne sais pas comment.</p>
+		<img style="float:right;" src="../assets/img/report_FR.png">
+		<ol>
+			<li>Cliquer sur un avatar. Un menu apparaîtra.</li>
+			<li>Choisir l'option <em>Ignorer</em>.</li>
+			<li>Il te sera impossible de voir ce que ce Habboo dit. Tu as changé d'avis et tu ne souhaites plus l'ignorer? Clique à nouveau sur l'avatar et choisis l'option <em>Écouter</em>.</li>
+		</ol>
+
+		<h3>Modérer un Habboo</h3>
+		<p><strong>Si tu es dans ton appart, ou dans un appart dans lequel tu as les droits</strong>, tu pourras décider qui peut visiter l'appart, et tu auras le pouvoir de mettre sous silence, expulser ou bloquer l'accès à d'autres utilisateurs. Cela te permet de jouer un rôle actif dans la modération générale de Habboo et de contribuer à créer une communauté plus saine et plus agréable.</p>
+
+		<h3>Rapporter un Habbo</h3>
+		<p>Si <strong>les choses tournent au vinaigre</strong>: des Habboos parlent de se rencontrer dans la vraie vie, veulent s'appeler par cam, échangent des données personnelles ou si quelqu'un est victime de harcèlement… Tu peux envisager de signaler ces personnes. Attention, utilise cet outil uniquement lorsque c'est nécessaire, lorsqu'une personne fait intentionnellement du mal aux autres ou à eux-mêmes.</p>
+		<ol>
+			<li>Clique sur l'avatar fauteur de troubles pour faire apparaître le menu.</li>
+			<li>Clique sur <em>Rapporter</em>.</li>
+			<li>Sélectionne les paroles à rapporter</li>
+			<li>Choisis une catégorie pour le problème</li>
+			<li>Explique ce qui se passe</li>
+			<li>Envoie ta demande et nous essaierons de résoudre le problème. Si tu choisis <em>Harcèlement</em> un modérateur pourra intervenir.</li>
+		</ol>
+		<p>Un autre moyen de rapporter un Habboo:</p>
+		<img style="float:right;" src="../assets/img/help_button_fr.png">
+		<ol>
+			<li>Clique sur <em>Aide</em> en haut à droite.</li>
+			<li>Choisis <em>Quelqu'un se comporte mal</em>. Tu verras alors la liste des Habboos dans la salle.</li>
+			<li>Choisis l'utilisateur en question.</li>
+			<li>Sélectionne le tchat à rapporter.</li>
+			<li>Choisis la meilleur description pour la situation.</li>
+			<li>Écris une courte description pour expliquer ce qui se passe.</li>
+			<li>Finalement clique sur <em>Envoyer la demande</em></li>
+		</ol>
+<hr>
+		<h2>Dans un message privé</h2>
+		<p>Si <strong>tu parles à quelqu'un en message privé et qu'il te met mal à l'aise</strong>:</p>
+		<ol>
+			<li>Clique sur <em>Signaler</em> se trouvant sous la tête du Habboo de la barre de Tchat.</li>
+			<li>On te demandera plus d'information sur ce qu'il s'est passé.</li>
+			<li>Nous prendrons la sanction adéquate.</li>
+		</ol>
+		<img src="../assets/img/report_im_fr.png" />
+<hr>
+		<h2>Dans un forum de groupe</h2><img style="float:right;" src="../assets/img/flag_3.png" />
+		<p>Tu peux <strong>signaler un sujet ou un commentaire inapproprié dans les forums de groupe</strong></p>
+		<ol>
+			<li>Clique sur le drapeau orange.</li>
+			<li>On te demandera plus d'information sur ce qu'il s'est passé.</li>
+			<li>Nous prendrons la sanction adéquate.</li>
+		</ol>
+
+
+
+
+	</div>
 	<div id='right'>aaaaaaaa</div>
 </div>
 
