f7507f2ceb3e85e313edc37c8fef22d550c848d9
Siignal <anthony.77181@hotmail.fr>
2017-04-06 16:40:45 +0200

    Add files via upload

--- habboo.php
+++ habboo.php
@@ -3,7 +3,39 @@ $pageid = '7'; ?>
 <?php include('template/header.php'); ?>
 
 <div id='content'>
-	<div id='left'></div>
+	<div id='left'>
+		   	<h1>HABBOO, C'EST QUOI?</h1>
+		   	<p><?= $site; ?> est une communauté virtuelle en ligne pixelisée sur laquelle tu pourras créer ton propre avatar, te faire des amis, discuter, créer des apparts, construire des jeux et participer aux animations officielles! Presque tout est possible sur <?= $site; ?>...</p>
+
+		   	<h1>PLUS QU'UN JEU…</h1><img style="float:right;padding:10px;" src="assets/img/ill_15.png" />
+		   	<p>Relooker ton avatar avec toutes les looks les plus fous n'est pas la seule façon de s'amuser sur <?= $site; ?>. Tu veux être l'architecte du siècle et construire des structures éblouissantes? Tu veux montrer tes compétences de fou de construction de jeu et émerveiller tes amis? Rejoins les concours! Es-tu fan de selfies et photos drôles? Notre appareil photo sera une diversion sans fin pour toi!</p>
+
+		   	<h1>TROUVE TA PLACE</h1>
+		   	<p>Tu aimes discuter et sortir avec tes amis? Les groupes <?= $site; ?>, forums et les jeux de rôles c'est la direction à prendre pour commencer. Rejoins l'armée pour faire ton devoir, enfiler ta cape et sauver l'univers. Inscris-toi et commence à explorer les possibilités infinies de rôle!</p>
+
+		   	<h1>EXPRIME-TOI</h1><img style="float:right;padding:10px;" src="assets/img/ill_16.png" />
+		   	<p>La créativité et l'originalité sont les bienvenues sur <?= $site; ?>! Chaque semaine, nous avons de supers compétitions que tu peux rejoindre. Des constructions d'apparts aux selfies, des vidéos style pixel-art et compétitions d'écriture - une tonne de trucs cools t'attendent pour laisser libre-court à ton élan artistique et gagner quelques winwin + prix! Tu es prêt? Consulte les news ur avoir plus d'infos sur la compétition du moment!</p>
+
+		   	<h1>JOUE GRATUITEMENT, POUR TOUJOURS.</h1>
+		   	<p><?= $site; ?> est un jeu gratuit en ligne donc tu peux explorer un vaste monde de salles, quêtes, discuter et gagner des prix, sans jamais avoir à payer!</p>
+			<p>Certains «extras» dans le jeu comme des mobiliers rares, des badges ainsi que l'adhésion au VIP Club peuvent être acheté avec des diamants Pour plus d'informations sur les suppléments dans le jeu, consulter <?= $site; ?> Shopping.</p>
+
+			<h1>TOUJOURS LÀ POUR AIDER…</h1><img style="float:right;padding:10px;" src="assets/img/ill_17.png" />
+			<p><?= $site; ?> est surveillé à longueur de journée par notre équipe de modération. Tu peux aussi faire beaucoup de choses pour rester en sécurité. Consulte les <a href="habboo/securite">Conseils de sécurité</a> pour savoir quoi faire.</p>
+			<p>Une équipe de guide est aussi régulièrement présente partout dans l'hôtel pour répondre à tes questions.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+	</div>
 	<div id='right'>aaaaaaaa</div>
 </div>
 
