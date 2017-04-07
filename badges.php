cc0e731749376f1a3f7ba563408c43358a05de37
Siignal <anthony.77181@hotmail.fr>
2017-04-07 19:25:11 +0200

    Add files via upload

--- badges.php
+++ badges.php
@@ -5,6 +5,18 @@ $pageid = '10'; ?>
 <div id='content'>
 	<div id='left'>
 
+		<div class="badge">
+			<div class="box__badge">
+				<img src="../assets/img/UK663.gif" class="shop__badge" />			
+				<div class="shop__glass"></div>
+			</div>
+			<a href=""><div class="shop__btn">Acheter</div></a>
+		</div>
+
+
+
+
+
 	</div>
 	<div id='right'>aaaaaaaa</div>
 </div>
--- packs.php
+++ packs.php
@@ -3,7 +3,68 @@ $pageid = '11';?>
 <?php include('template/header.php'); ?>
 
 <div id='content'>
-	<div id='left'></div>
+	<div id='left'>
+		
+		<div class="pack">
+			<div class="pack__ortitle">Pack Or</div>
+			<div class="pack__orcontent">
+				<div style="text-align:center;">Ce pack comporte :</div>
+				<ol>
+					<li class="duckets">1000 duckets</li>
+					<li class="respects">100 respects</li>
+					<li class="winwins">500 winwins</li>
+				</ol>
+
+				<div style="text-align:center;">
+				<img class="pack__badge" src="../assets/img/BR994.gif" />
+				<img class="pack__badge" src="../assets/img/UK663.gif" />
+				</div>
+				<br>
+				<a href=""><div class="pack__orbtn">175 diamants</div></a>
+			</div>
+		</div>
+
+		<div class="pack">
+			<div class="pack__argenttitle">Pack Argent</div>
+			<div class="pack__argentcontent">
+				<div style="text-align:center;">Ce pack comporte :</div>
+				<ol>
+					<li class="duckets">700 duckets</li>
+					<li class="respects">75 respects</li>
+					<li class="winwins">350 winwins</li>
+				</ol>
+
+				<div style="text-align:center;">
+				<img class="pack__badge" src="../assets/img/BR995.gif" />
+				</div>
+				<br>
+				<a href=""><div class="pack__argentbtn">150 diamants</div></a>
+			</div>
+		</div>		
+
+		<div class="pack">
+			<div class="pack__bronzetitle">Pack Bronze</div>
+			<div class="pack__bronzecontent">
+				<div style="text-align:center;">Ce pack comporte :</div>
+				<ol>
+					<li class="duckets">400 duckets</li>
+					<li class="respects">100 respects</li>
+					<li class="winwins">250 winwins</li>
+				</ol>
+
+				<div style="text-align:center;">
+				<img class="pack__badge" src="../assets/img/BR996.gif" />
+				</div>
+				<br>
+				<a href=""><div class="pack__bronzebtn">100 diamants</div></a>
+			</div>
+		</div>
+
+
+
+
+
+	</div>
 	<div id='right'>aaaaaaaa</div>
 </div>
 
