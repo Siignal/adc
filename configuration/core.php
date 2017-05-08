c0877a662a59b8c2648ec77e7c4f1e36b7c7d324
Siignal <anthony.77181@hotmail.fr>
2017-05-08 20:58:45 +0200

    Delete core.php

--- configuration/core.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    $site = 'Habboo';
-    $url = 'http://localhost/habbo/';
-    $version = '1.0';
-    $assets = 'http://localhost/habbo/assets/';
-    $developpeurs = 'Grégoire & Siignal';
-
-?>
